c42cd0aed110f36927c165d3655b70edf5979336
wenxinsir <1847645807@qq.com>
2016-10-14 15:43:10 +0800

    添加跳转的工具类，并在闪屏中使用

---
+++ app/src/main/java/cn/ucai/fulicenter/activity/utils/MFGT.java
@@ -0,0 +1,28 @@
+package cn.ucai.fulicenter.activity.utils;
+
+import android.app.Activity;
+import android.content.Intent;
+
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.activity.MainActivity;
+
+/**
+ * Created by Administrator on 2016/10/14.
+ */
+public class MFGT {
+    public static void finish(Activity activity) {
+        activity.finish();
+        activity.overridePendingTransition(R.anim.push_right_in, R.anim.push_right_out);
+    }
+
+    public static void gotoMainActivity(Activity context) {
+        startActivity(context, MainActivity.class);
+    }
+
+    public static void startActivity(Activity context, Class<?> cls) {
+        Intent intent = new Intent();
+        intent.setClass(context, cls);
+        context.startActivity(intent);
+        context.overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+    }
+}

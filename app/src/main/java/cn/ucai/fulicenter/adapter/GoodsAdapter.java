83a6c82b3b910de74c95de02eafe793674642805
wenxinsir <1847645807@qq.com>
2016-10-17 13:27:53 +0800

    添加的商品的adapter的基本框架

---
+++ app/src/main/java/cn/ucai/fulicenter/adapter/GoodsAdapter.java
@@ -0,0 +1,94 @@
+package cn.ucai.fulicenter.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.support.v7.widget.RecyclerView.Adapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.NewGoodsBean;
+
+/**
+ * Created by Administrator on 2016/10/17.
+ */
+public class GoodsAdapter extends Adapter {
+    Context mContext;
+    List<NewGoodsBean> mList;
+
+    public GoodsAdapter(Context mContext, List<NewGoodsBean> mList) {
+        this.mContext = mContext;
+        this.mList = mList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        ViewHolder holder = null;
+        if (viewType == I.TYPE_FOOTER) {
+            holder = new FooterViewHolder(View.inflate(mContext, R.layout.item_footer, null));
+        } else {
+            holder = new GoodsViewHolder(View.inflate(mContext, R.layout.item_goods, null));
+        }
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        if (getItemViewType(position) == I.TYPE_FOOTER) {
+
+        } else {
+            GoodsViewHolder vh = (GoodsViewHolder) holder;
+            NewGoodsBean goods = mList.get(position);
+            //set image
+            vh.tvGoods.setText(goods.getGoodsName());
+            vh.tvmoney.setText(goods.getCurrencyPrice());
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mList != null ? mList.size() + 1 : 1;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (position == getItemCount() - 1) {
+            return I.TYPE_FOOTER;
+        }
+        return I.TYPE_ITEM;
+    }
+
+    static class GoodsViewHolder extends ViewHolder {
+        @Bind(R.id.ivGoodsTh)
+        ImageView ivGoodsTh;
+        @Bind(R.id.tvGoods)
+        TextView tvGoods;
+        @Bind(R.id.tvmoney)
+        TextView tvmoney;
+        @Bind(R.id.layout_goods)
+        LinearLayout layoutGoods;
+
+        GoodsViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+
+    static class FooterViewHolder extends ViewHolder {
+        @Bind(R.id.tvFooter)
+        TextView tvFooter;
+
+        FooterViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+        }
+    }
+}

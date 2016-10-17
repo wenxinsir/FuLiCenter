5872ceee96bd8e28e77f27ec38535f859c92bc83
wenxinsir <1847645807@qq.com>
2016-10-17 18:52:19 +0800

    添加新品下载的网络请求的方法

---
+++ app/src/main/java/cn/ucai/fulicenter/net/NetDao.java
@@ -0,0 +1,39 @@
+package cn.ucai.fulicenter.net;
+
+import android.content.Context;
+
+import cn.ucai.fulicenter.I;
+import cn.ucai.fulicenter.bean.NewGoodsBean;
+
+/**
+ * Created by Administrator on 2016/10/17.
+ */
+public class NetDao {
+    public static void downloadNewGoods(Context context, int pageId, OkHttpUtils.OnCompleteListener<NewGoodsBean> listener) {
+        OkHttpUtils utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_FIND_NEW_BOUTIQUE_GOODS)
+                .addParam(I.NewAndBoutiqueGoods.CAT_ID, String.valueOf(I.CAT_ID))
+                .addParam(I.PAGE_ID, String.valueOf(pageId))
+                .addParam(I.PAGE_SIZE, String.valueOf(I.PAGE_SIZE_DEFAULT))
+                .targetClass(NewGoodsBean[].class)
+                .execute(listener);
+    }
+
+    public void temp(Context context) {
+        OkHttpUtils utils = new OkHttpUtils(context);
+        utils.setRequestUrl(I.REQUEST_FIND_NEW_BOUTIQUE_GOODS)
+                .addParam(I.PAGE_SIZE, String.valueOf(20))
+                .targetClass(NewGoodsBean.class)
+                .execute(new OkHttpUtils.OnCompleteListener() {
+                    @Override
+                    public void onSuccess(Object result) {
+
+                    }
+
+                    @Override
+                    public void onError(String error) {
+
+                    }
+                });
+    }
+}

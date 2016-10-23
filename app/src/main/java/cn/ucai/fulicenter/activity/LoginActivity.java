8825bdaa086be3a8456d1eb2dd493d7aa8fadc94
wenxinsir <1847645807@qq.com>
2016-10-23 21:53:39 +0800

    用户登录

--- app/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
+++ app/src/main/java/cn/ucai/fulicenter/activity/LoginActivity.java
@@ -1,7 +1,11 @@
 package cn.ucai.fulicenter.activity;
 
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
 
@@ -10,19 +14,31 @@ import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.fulicenter.I;
 import cn.ucai.fulicenter.R;
+import cn.ucai.fulicenter.bean.Result;
+import cn.ucai.fulicenter.bean.User;
+import cn.ucai.fulicenter.net.NetDao;
+import cn.ucai.fulicenter.utils.CommonUtils;
+import cn.ucai.fulicenter.utils.L;
 import cn.ucai.fulicenter.utils.MFGT;
+import cn.ucai.fulicenter.utils.OkHttpUtils;
 
 public class LoginActivity extends BaseActivity {
+    private  static final String TAG = LoginActivity.class.getSimpleName();
 
     @Bind(R.id.username)
     EditText mUsername;
     @Bind(R.id.password)
     EditText mPassword;
 
+    String username ;
+    String password ;
+    LoginActivity mContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
+        mContent = this;
         super.onCreate(savedInstanceState);
     }
 
@@ -45,11 +61,65 @@ public class LoginActivity extends BaseActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.btn_login:
+                checkedInput();
                 break;
             case R.id.btn_register:
                 MFGT.gotoRegister(this);
                 break;
         }
+    }
+
+    private void checkedInput() {
+        username = mUsername.getText().toString().trim();
+        password = mPassword.getText().toString().trim();
+        if (TextUtils.isEmpty(username)){
+            CommonUtils.showLongToast(R.string.user_name_connot_be_empty);
+            mUsername.requestFocus();
+            return;
+        }else if(TextUtils.isEmpty(password)){
+            mPassword.requestFocus();
+            return;
+        }
+        login();
+        }
+
+    private void login() {
+        final ProgressDialog pd = new ProgressDialog(mContent);
+        pd.setMessage(getResources().getString(R.string.logining));
+        pd.show();
+        L.e(TAG,"username="+username+",password="+password);
+        NetDao.login(mContent, username, password, new OkHttpUtils.OnCompleteListener<Result>() {
+            @Override
+            public void onSuccess(Result result) {
+                pd.dismiss();
+                L.e(TAG,"result = "+ result);
+                if (result == null){
+                    CommonUtils.showLongToast(R.string.login_fail);
+                }else {
+                    if (result.isRetMsg()){
+                        User user = (User) result.getRetData();
+                        L.e(TAG,"User = "+user);
+                        MFGT.finish(mContent);
+                    }else {
+                        if (result.getRetCode() == I.MSG_LOGIN_UNKNOW_USER){
+                            CommonUtils.showLongToast(R.string.login_fail_unknow_user);
+                        }else if (result.getRetCode() == I.MSG_LOGIN_ERROR_PASSWORD){
+                            CommonUtils.showLongToast(R.string.login_fail_error_password);
+                        }else {
+                            CommonUtils.showLongToast(R.string.login_fail);
+                        }
+                    }
+                }
+
+            }
+
+            @Override
+            public void onError(String error) {
+                CommonUtils.showLongToast(error);
+                L.e(TAG,"error="+error);
+
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/cn/ucai/fulicenter/bean/User.java
@@ -0,0 +1,83 @@
+package cn.ucai.fulicenter.bean;
+
+/**
+ * Created by Administrator on 2016/10/23.
+ */
+public class User {
+    private String muserName;
+    private String myserNick;
+    private int mavatarId;
+    private String mavatarPath;
+    private String mavatarSuffix;
+    private int mavatarType;
+    private String mavatarLastUpdateTime;
+
+    public String getMuserName() {
+        return muserName;
+    }
+
+    public void setMuserName(String muserName) {
+        this.muserName = muserName;
+    }
+
+    public String getMyserNick() {
+        return myserNick;
+    }
+
+    public void setMyserNick(String myserNick) {
+        this.myserNick = myserNick;
+    }
+
+    public int getMavatarId() {
+        return mavatarId;
+    }
+
+    public void setMavatarId(int mavatarId) {
+        this.mavatarId = mavatarId;
+    }
+
+    public String getMavatarSuffix() {
+        return mavatarSuffix;
+    }
+
+    public void setMavatarSuffix(String mavatarSuffix) {
+        this.mavatarSuffix = mavatarSuffix;
+    }
+
+    public String getMavatarPath() {
+        return mavatarPath;
+    }
+
+    public void setMavatarPath(String mavatarPath) {
+        this.mavatarPath = mavatarPath;
+    }
+
+    public int getMavatarType() {
+        return mavatarType;
+    }
+
+    public void setMavatarType(int mavatarType) {
+        this.mavatarType = mavatarType;
+    }
+
+    public String getMavatarLastUpdateTime() {
+        return mavatarLastUpdateTime;
+    }
+
+    public void setMavatarLastUpdateTime(String mavatarLastUpdateTime) {
+        this.mavatarLastUpdateTime = mavatarLastUpdateTime;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "muserName='" + muserName + '\'' +
+                ", myserNick='" + myserNick + '\'' +
+                ", mavatarId=" + mavatarId +
+                ", mavatarPath='" + mavatarPath + '\'' +
+                ", mavatarSuffix='" + mavatarSuffix + '\'' +
+                ", mavatarType=" + mavatarType +
+                ", mavatarLastUpdateTime='" + mavatarLastUpdateTime + '\'' +
+                '}';
+    }
+}
